02f5d3e96d46ca940547cb97fca44d59575708da
Amir <soltamir194@gmail.com>
2023-12-19 10:13:20 -0500

    upd

--- src/lib/server/clients/redis.ts
+++ src/lib/server/clients/redis.ts
@@ -8,6 +8,9 @@ if(process.env.REDIS_URL == null){
     throw new Error('missing REDIS_URL');
 }
 
+setTimeout(function(){
+    console.log("Executed after 1 second");
+}, 1000);
 
 export const redis = new Redis(process.env.REDIS_URL);
 
