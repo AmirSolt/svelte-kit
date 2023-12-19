77766ff9de108a6304e26d61beb55e71441b7035
Amir <soltamir194@gmail.com>
2023-12-19 09:44:31 -0500

    upd

--- src/lib/server/clients/prisma.ts
+++ src/lib/server/clients/prisma.ts
@@ -2,7 +2,16 @@ import { PrismaClient } from '@prisma/client'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const prisma = new PrismaClient()
+
+if(process.env.DATABASE_URL == null){
+    throw new Error('missing DATABASE_URL');
+}
+
+const dburl:string = process.env.DATABASE_PRIVATE_URL || process.env.DATABASE_URL;
+
+console.log("dburl",dburl)
+
+export const prisma = new PrismaClient({datasourceUrl:dburl})
 
 
 // npx prisma migrate dev --name init
--- src/lib/server/clients/redis.ts
+++ src/lib/server/clients/redis.ts
@@ -2,11 +2,13 @@ import Redis from 'ioredis';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+
+
 if(process.env.REDIS_URL == null){
     throw new Error('missing REDIS_URL');
 }
 
-const redisurl = process.env.REDIS_URL;
+const redisurl:string = process.env.REDIS_PRIVATE_URL || process.env.REDIS_URL;
 
 if (!redisurl) {
     throw Error("You are missing one of the variables needed to send a message")
