2512a890cdf5025f35ccb0c2473a38279c4e7efe
Amir <soltamir194@gmail.com>
2023-12-20 06:22:15 -0500

    upd

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+</script>
+
+<div class="">
+    <div class="flex flex-col justify-center items-center text-center gap-8 pt-16">
+        <div class="flex flex-col justify-center items-center text-center gap-2">
+            <h1 class="text-6xl">
+                Product
+                <strong class="text-6xl md:text-6xl  font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-purple-500 via-30% to-pink-500 to-90%">
+                    Analyzer
+                </strong> 
+            </h1>
+            <div>
+                <p class="text-2xl">
+                    Analyze your options before purchase
+                </p>
+            </div>
+        </div>
+        <a href="http://m.me/177877642083248" class="btn text-2xl variant-filled-primary" target="_blank" rel="noreferrer">
+            Go to Messenger
+        </a>
+    </div>
+</div>
